2f9a12bda6ca07e3fda7b767cbdde422759ae535
Fedulov Danil <danya7777kg@gmail.com>
2026-02-14 13:46:41 +0600

    init express App

---
+++ index.ts
@@ -0,0 +1,15 @@
+import express, {Request, Response} from 'express';
+
+const app = express();
+const port = 8000;
+
+app.get("/", (req: Request, res: Response) => {
+    res.send("Hello");
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port: ${port}`);
+});
+
+
+
